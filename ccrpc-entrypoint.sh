34b1c6a28b328905fcf2ce95549222f0f185e35e
Matt Yoder <myoder@ccrpc.org>
2018-11-07 09:26:23 -0600

    Fixed backup directory permissions.

--- ccrpc-entrypoint.sh
+++ ccrpc-entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 mkdir -p /opt/backup/base
+chown -R "$(id -u)" /opt/backup/base
+chmod 700 /opt/backup/base
+
 mkdir -p /opt/backup/wal
+chown -R "$(id -u)" /opt/backup/wal
+chmod 700 /opt/backup/wal
 
 /usr/local/bin/docker-entrypoint.sh "$@"
